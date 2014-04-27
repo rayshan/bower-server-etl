6c8b055e9dccc782b2dae0c94400b8955936181d
Ray Shan <ray@rayshan.com>
2014-04-27 10:31:29 -0700

    didn't define stop script -_-

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-#set -o errexit
-#set -o pipefail
+set -o errexit
+set -o pipefail
 
 printf "WIP: redis for $APP_NAME node app stopping...\n"
 redis-cli shutdown
 
 printf "WIP: $APP_NAME node app stopping...\n"
-#if [[ "$HOSTNAME" = "shan.io" ]]; then
-#  forever list >> $APP_DIR/$APP_LOG
-#  forever stop index.coffee >> $APP_DIR/$APP_LOG
-#  forever list >> $APP_DIR/$APP_LOG
-#fi
-
-forever list >> $APP_DIR/$APP_LOG
-forever stop index.coffee >> $APP_DIR/$APP_LOG
-forever list >> $APP_DIR/$APP_LOG
+if [[ "$HOSTNAME" = "shan.io" ]]; then
+  forever list >> $APP_DIR/$APP_LOG
+  forever stop index.coffee >> $APP_DIR/$APP_LOG
+  forever list >> $APP_DIR/$APP_LOG
+fi
