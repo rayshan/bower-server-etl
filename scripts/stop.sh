06d0505bdcfb681e38d4c1c8c219696410a211bd
Ray Shan <ray@rayshan.com>
2014-04-27 10:13:49 -0700

    tweaked stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-set -o errexit
-set -o pipefail
+#set -o errexit
+#set -o pipefail
 
-#printf "WIP: redis for $APP_NAME node app stopping...\n"
-#redis-cli shutdown
+printf "WIP: redis for $APP_NAME node app stopping...\n"
+redis-cli shutdown
 
 printf "WIP: $APP_NAME node app stopping...\n"
-if [[ "$HOSTNAME" = "shan.io" ]]; then
-  forever list >> $APP_DIR/$APP_LOG
-  forever stop index.coffee >> $APP_DIR/$APP_LOG
-  forever list >> $APP_DIR/$APP_LOG
-fi
+#if [[ "$HOSTNAME" = "shan.io" ]]; then
+#  forever list >> $APP_DIR/$APP_LOG
+#  forever stop index.coffee >> $APP_DIR/$APP_LOG
+#  forever list >> $APP_DIR/$APP_LOG
+#fi
+
+forever list >> $APP_DIR/$APP_LOG
+forever stop index.coffee >> $APP_DIR/$APP_LOG
+forever list >> $APP_DIR/$APP_LOG
