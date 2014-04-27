872e65d6a0e770b71e88d3738b6d042cda25185b
Ray Shan <ray@rayshan.com>
2014-04-26 21:50:18 -0700

    tweaked deployment scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,19 +3,19 @@
 set -o errexit
 set -o pipefail
 
-printf "WIP: redis for $APP_NAME node app starting...\n"
-redis-server ./scripts/redis.conf
-
-printf "WIP: $APP_NAME node app starting...\n"
 export APP_PORT=3000
-printf "host is $HOSTNAME\n"
 if [[ "$HOSTNAME" = "shan.io" ]]; then
+  printf "WIP: redis for $APP_NAME node app starting...\n"
+  redis-server $APP_DIR/scripts/redis.conf
   export NODE_ENV=prod
   export APP_GA_KEY_PATH=/var/www/_keys/2f798d5414685a52456c31158c9fa61fa14256c3-privatekey.pem
+  printf "WIP: $APP_NAME node app starting...\n"
   forever start --append -l $APP_DIR/$APP_LOG --minUptime 1000 --spinSleepTime 10000 -c coffee index.coffee >> $APP_DIR/$APP_LOG
+  forever list >> $APP_DIR/$APP_LOG
 else
   source ./scripts/dev.sh
   export NODE_ENV=dev
   export APP_GA_KEY_PATH=~/=Projects=/2f798d5414685a52456c31158c9fa61fa14256c3-privatekey.pem
+  redis-server ./scripts/redis.conf
   node-dev index.coffee
 fi
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,5 +8,7 @@ redis-cli shutdown
 
 printf "WIP: $APP_NAME node app stopping...\n"
 if [[ "$HOSTNAME" = "shan.io" ]]; then
+  forever list >> $APP_DIR/$APP_LOG
   forever stop index.coffee >> $APP_DIR/$APP_LOG
+  forever list >> $APP_DIR/$APP_LOG
 fi
