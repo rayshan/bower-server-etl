b0382461425f5141496c8205f961b1e1c78f193d
Daniel Mane <danmane@gmail.com>
2014-07-29 13:37:45 -0700

    [feat] Show 7-day moving average of data, update chart title appropriately [fix] Remove padding on the xScale [fix] Give legend its own space on top, ensuring it never overlaps data [refactor] clean out unused css

--- public/b-chart-users/b-chart-users.coffee
+++ public/b-chart-users/b-chart-users.coffee
@@ -1,14 +1,39 @@
 module = angular.module 'B.Chart.Users', []
 
+movingAverage = (dataArr, numDaysToAverage, accessor) ->
+  stack = []
+  out = []
+  for d in dataArr
+    x = if accessor? then accessor(d) else d
+    stack.unshift(x)
+    if stack.length == numDaysToAverage
+      avg = d3.sum(stack) / numDaysToAverage
+      out.push(avg)
+      stack.pop()
+  out
+
 module.service 'bChartUserData', ($q, bDataSvc, d3) ->
+  parseArray = (dataArr) ->
+    # in: [key, dateStr, val]
+    # out: {key: str, date: date, val: num, movingAvg: num}
+    mAvg = movingAverage(dataArr, 7, (d) -> d[2])
+    parseDate = d3.time.format("%Y%m%d").parse # e.g. 20140301
+    dataArr.slice(6).map((d, i) -> {
+      key: d[0],
+      date: parseDate(d[1]),
+      val: d[2],
+      movingAvg: mAvg[i]}
+    )
+
   parseData = (data) ->
     _deferred = $q.defer()
 
     data = data.data.users
-    parseDate = d3.time.format("%Y%m%d").parse # e.g. 20140301
-    data.forEach (d) -> d[1] = parseDate d[1]; return # date
     data = d3.nest().key((d) -> d[0]).entries data # group by key; apply to data
-
+    newUsersData      = parseArray data[0].values
+    existingUsersData = parseArray data[1].values
+    npmInstallsData   = parseArray data[2].values
+    data = [newUsersData, existingUsersData, npmInstallsData]
     _deferred.resolve
       data: data
     _deferred.promise
@@ -21,43 +46,45 @@ module.directive "bChartUsers", (d3, bChartUserData) ->
   link: (scope, ele) ->
     render = (data) ->
       stack = d3.layout.stack()
-        .values (d) -> d.values
-        .x (d) -> d[1]
-        .y (d) -> d[2]
+        .values (d) -> d
+        .x (d) -> d.date
+        .y (d) -> d.movingAvg
         .order "reverse"
 
       usersData = data.data.slice(0, 2)
       npmData = data.data[2]
       stackedData = stack usersData
-      newUsersData = stackedData[0].values
-      existingUsersData = stackedData[1].values
+      newUsersData = stackedData[0]
+      existingUsersData = stackedData[1]
 
       xScale = new Plottable.Scale.Time()
+      xScale.domainer(new Plottable.Domainer().pad(0)) # Disable auto-padding on the scale
       yScale = new Plottable.Scale.Linear()
       colorScale = new Plottable.Scale.Color().domain(["New Users", "Existing Users"]).range(["#00acee", "#ffcc2f"])
-
       xAxis = new Plottable.Axis.Time(xScale, "bottom")
       yAxis = new Plottable.Axis.Numeric(yScale, "left")
 
       addY = (d) -> d.y0 + d.y
       area_existing = new Plottable.Plot.Area(existingUsersData, xScale, yScale)
-        .project("x", "1", xScale)
+        .project("x", "date", xScale)
         .project("y0", "y0", yScale)
         .project("y", addY, yScale)
-        .project("fill-opacity", 1)
-        .project("fill", () -> "#ffcc2f")
+        .classed("existing-users", true)
 
       area_new = new Plottable.Plot.Area(newUsersData, xScale, yScale)
-        .project("x", "1", xScale)
+        .project("x", "date", xScale)
         .project("y0", "y0", yScale)
         .project("y", addY, yScale)
-        .project("fill", () -> "#00acee")
-        .project("fill-opacity", 1)
+        .classed("new-users", true);
 
       gridlines = new Plottable.Component.Gridlines(xScale, yScale)
       legend = new Plottable.Component.Legend(colorScale)
-      center = gridlines.merge(area_existing).merge(area_new).merge(legend)
-      chart = new Plottable.Template.StandardChart().center(center).xAxis(xAxis).yAxis(yAxis)
+      center = area_existing.merge(area_new).merge(gridlines)
+      chart = new Plottable.Component.Table([
+          [null, legend],
+          [yAxis, center],
+          [null,  xAxis]
+        ]);
       chart.renderTo("#users-chart")
 
       return
--- public/b-chart-users/b-chart-users.js
+++ public/b-chart-users/b-chart-users.js
@@ -1,21 +1,53 @@
 (function() {
-  var module;
+  var module, movingAverage;
 
   module = angular.module('B.Chart.Users', []);
 
+  movingAverage = function(dataArr, numDaysToAverage, accessor) {
+    var avg, d, out, stack, x, _i, _len;
+    stack = [];
+    out = [];
+    for (_i = 0, _len = dataArr.length; _i < _len; _i++) {
+      d = dataArr[_i];
+      x = accessor != null ? accessor(d) : d;
+      stack.unshift(x);
+      if (stack.length === numDaysToAverage) {
+        avg = d3.sum(stack) / numDaysToAverage;
+        out.push(avg);
+        stack.pop();
+      }
+    }
+    return out;
+  };
+
   module.service('bChartUserData', ["$q", "bDataSvc", "d3", function($q, bDataSvc, d3) {
-    var parseData;
+    var parseArray, parseData;
+    parseArray = function(dataArr) {
+      var mAvg, parseDate;
+      mAvg = movingAverage(dataArr, 7, function(d) {
+        return d[2];
+      });
+      parseDate = d3.time.format("%Y%m%d").parse;
+      return dataArr.slice(6).map(function(d, i) {
+        return {
+          key: d[0],
+          date: parseDate(d[1]),
+          val: d[2],
+          movingAvg: mAvg[i]
+        };
+      });
+    };
     parseData = function(data) {
-      var parseDate, _deferred;
+      var existingUsersData, newUsersData, npmInstallsData, _deferred;
       _deferred = $q.defer();
       data = data.data.users;
-      parseDate = d3.time.format("%Y%m%d").parse;
-      data.forEach(function(d) {
-        d[1] = parseDate(d[1]);
-      });
       data = d3.nest().key(function(d) {
         return d[0];
       }).entries(data);
+      newUsersData = parseArray(data[0].values);
+      existingUsersData = parseArray(data[1].values);
+      npmInstallsData = parseArray(data[2].values);
+      data = [newUsersData, existingUsersData, npmInstallsData];
       _deferred.resolve({
         data: data
       });
@@ -33,18 +65,19 @@
         render = function(data) {
           var addY, area_existing, area_new, center, chart, colorScale, existingUsersData, gridlines, legend, newUsersData, npmData, stack, stackedData, usersData, xAxis, xScale, yAxis, yScale;
           stack = d3.layout.stack().values(function(d) {
-            return d.values;
+            return d;
           }).x(function(d) {
-            return d[1];
+            return d.date;
           }).y(function(d) {
-            return d[2];
+            return d.movingAvg;
           }).order("reverse");
           usersData = data.data.slice(0, 2);
           npmData = data.data[2];
           stackedData = stack(usersData);
-          newUsersData = stackedData[0].values;
-          existingUsersData = stackedData[1].values;
+          newUsersData = stackedData[0];
+          existingUsersData = stackedData[1];
           xScale = new Plottable.Scale.Time();
+          xScale.domainer(new Plottable.Domainer().pad(0));
           yScale = new Plottable.Scale.Linear();
           colorScale = new Plottable.Scale.Color().domain(["New Users", "Existing Users"]).range(["#00acee", "#ffcc2f"]);
           xAxis = new Plottable.Axis.Time(xScale, "bottom");
@@ -52,16 +85,12 @@
           addY = function(d) {
             return d.y0 + d.y;
           };
-          area_existing = new Plottable.Plot.Area(existingUsersData, xScale, yScale).project("x", "1", xScale).project("y0", "y0", yScale).project("y", addY, yScale).project("fill-opacity", 1).project("fill", function() {
-            return "#ffcc2f";
-          });
-          area_new = new Plottable.Plot.Area(newUsersData, xScale, yScale).project("x", "1", xScale).project("y0", "y0", yScale).project("y", addY, yScale).project("fill", function() {
-            return "#00acee";
-          }).project("fill-opacity", 1);
+          area_existing = new Plottable.Plot.Area(existingUsersData, xScale, yScale).project("x", "date", xScale).project("y0", "y0", yScale).project("y", addY, yScale).classed("existing-users", true);
+          area_new = new Plottable.Plot.Area(newUsersData, xScale, yScale).project("x", "date", xScale).project("y0", "y0", yScale).project("y", addY, yScale).classed("new-users", true);
           gridlines = new Plottable.Component.Gridlines(xScale, yScale);
           legend = new Plottable.Component.Legend(colorScale);
-          center = gridlines.merge(area_existing).merge(area_new).merge(legend);
-          chart = new Plottable.Template.StandardChart().center(center).xAxis(xAxis).yAxis(yAxis);
+          center = area_existing.merge(area_new).merge(gridlines);
+          chart = new Plottable.Component.Table([[null, legend], [yAxis, center], [null, xAxis]]);
           chart.renderTo("#users-chart");
         };
         bChartUserData.then(render);
